b498509d25d14bcf3c772f47f2d18fbc6425418a
Nikita Mensky <nmensky@gmail.com>
2025-04-20 00:57:55 +0300

    first commit in project

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+
+import { Layout } from 'antd';
+
+const headerStyle = {
+    textAlign: 'center',
+    color: '#fff',
+    height: 60,
+    paddingInline: 48,
+    lineHeight: '64px',
+    backgroundColor: '#4096ff',
+};
+
+const contentStyle= {
+    textAlign: 'center',
+    minHeight: 'calc(100vh - 60px)',
+    color: '#fff',
+    backgroundColor: '#002c74',
+};
+
+const siderStyle = {
+    textAlign: 'center',
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#1677ff',
+};
+
+
+export default function App() {
+
+    return (
+        <Layout>
+            <Layout.Header style={headerStyle}>Header</Layout.Header>
+            <Layout>
+                <Layout.Sider width="25%" style={siderStyle}>
+                    Sider
+                </Layout.Sider>
+                <Layout.Content style={contentStyle}>Content</Layout.Content>
+            </Layout>
+        </Layout>
+    )
+}
+
+
